6f4706d85dc6f0ab08b3314e9449a1d24b70b440
rajachapuri <rajachapuri@WSAMZN-DMUE29AG>
2025-05-14 16:09:55 -0400

    Sample Springboot Project

---
+++ src/main/java/com/guidehouse/armymsd/ArmymsdApplication.java
@@ -0,0 +1,13 @@
+package com.guidehouse.armymsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmymsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmymsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guidehouse/armymsd/ArmymsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidehouse.armymsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmymsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
